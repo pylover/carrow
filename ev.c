dfc02a08a1d9e747559826fd70c0ebd3c579e05d
pylover <vahid.mardani@gmail.com>
2023-03-27 04:13:53 +0330

    Implementing: ev

--- ev.c
+++ ev.c
@@ -1,9 +1,87 @@
 #include "ev.h"
 
+#include <clog.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+
 
 struct evbag {
     int fd;
     struct circuitA *circuit;
     void *state;
+    int index;
 };
 
+
+struct evstate {
+    int fd;
+    unsigned int openmax;
+    unsigned int bagscount;
+    struct evbag **bags;
+};
+
+
+void 
+evinitA(struct circuitA *c, void *s, struct ev *priv) {
+    if (priv->state != NULL ) {
+        errorA(c, s, "Already initialized");
+        return;
+    }
+   
+    priv->state = malloc(sizeof(struct evstate));
+    if (priv->state == NULL) {
+        FATAL("Out of Memory");
+    }
+    
+    struct evstate *state = priv->state;
+    state->fd = epoll_create1(0);
+    if (state->fd < 0) {
+        errorA(c, s, "epoll_create1");
+        return;
+    }
+
+    long openmax = sysconf(_SC_OPEN_MAX);
+    if (openmax == -1) {
+        errorA(c, s, "sysconf(openmax)");
+        return;
+    }
+    state->openmax = openmax;
+    state->bagscount = 0;
+    state->bags = calloc(openmax, sizeof(struct evbag*));
+    returnA(c, s);
+}
+
+
+static void
+_bags_freeall(struct evstate *state) {
+    int i;
+    struct evbag *bag;
+
+    for (i = 0; i < state->openmax; i++) {
+        bag = state->bags[i];
+
+        if (bag == NULL) {
+            continue;
+        }
+
+        free(bag);
+        state->bags[i] = NULL;
+    }
+}
+
+
+static void 
+evclose(struct evstate *state) {
+    close(state->fd);
+    _bags_freeall(state);
+    free(state->bags);
+    free(state);
+}
+
+
+void 
+evcloseA(struct circuitA *c, void *s, struct ev *priv) {
+    evclose(priv->state);
+}
--- ev.h
+++ ev.h
@@ -2,9 +2,24 @@
 #define CARROW_EV_H
 
 
-struct evbag;
+#include "core.h"
 
 
+struct evstate;
+
+
+struct ev {
+    int flags;
+    struct evstate *state;
+};
+
+
+void 
+evinitA(struct circuitA *c, void *s, struct ev *priv);
+
+
+void 
+evcloseA(struct circuitA *c, void *s, struct ev *priv);
 
 
 #endif
